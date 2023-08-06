b1a11977ced0ae22f8f37f65293172318f765b65
Nina Cliville <nina.cliville@gmail.com>
2023-08-06 15:24:35 +0200

    Authentification utilisateur

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,17 +1,14 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const SECRET_TOKEN = process.env.SECRET_TOKEN;
-
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, SECRET_TOKEN);
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
         };
-    next();
-    } catch(error) {
-        res.status(403).json({ error });
-    };
+        next();
+    } catch (error) {
+        res.status(401).json({ error });
+    }
 };
